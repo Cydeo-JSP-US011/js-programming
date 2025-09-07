6cefcbbc8dc88dcaea86e9aa59e15ef74408ede9
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-09-07 12:17:53 -0400

    JS Programming

--- reviews/JSReviews.js
+++ reviews/JSReviews.js
@@ -91,4 +91,81 @@ console.log(actualTitle === expectedTitle);
 
 console.log(actualTitle.toLowerCase() === expectedTitle.toLowerCase()); // true
 
-// come back at 11:40 AM EST
+
+console.log("------------------------------");
+
+let elements = [10, 20, 30, 40, 50, 60];
+
+console.log(elements);
+
+// console.log(elements[0]);
+
+// create for of loop to access each elemenst of the array.
+for (let element of elements) {
+
+    console.log(element);
+
+}
+
+console.log("------------------------------");
+
+// use for of loop to print each element of the array in reverse order.
+for (let i = elements.length - 1; i >= 0; i--) {
+
+    console.log(elements[i]);
+
+}
+
+console.log("------------------------------");
+
+let maps = {
+    "name": "John Doe",
+     "age": 30, 
+     "city": "New York"
+};
+
+console.log(maps);
+
+for (let key in maps) {
+    console.log(`${key}: ${maps[key]}`);
+}
+
+console.log("------------------------------");
+
+let mySet = new Set([1, 2, 3, 2, 4, 5, 3, 6, 1, 7]);
+
+console.log(mySet);
+
+for (let element of mySet) {
+    console.log(element);
+}
+
+console.log("------------------------------");
+
+/*
+Create a function that can reverse a string.
+     The function should not using any built in reverse function or method.
+*/
+/**
+ * Reverses a string by iterating through its characters from end to beginning.
+ * This function does not use any built-in reverse methods or functions.
+ * 
+ * @param {string} str - The input string to be reversed
+ * @returns {string} A new string with characters in reverse order of the input string
+ */
+function reverseString(str) {
+    let reversedStr = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+
+    return reversedStr;
+}
+
+
+// test the functions.
+console.log(reverseString("Hello World!")); // "!dlroW olleH"
+
+console.log("------------------------------");
+
